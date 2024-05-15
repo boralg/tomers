f939aebc24092b70e96ab3e87998bb223b0f4bca
thinnerthinker <thinnrthinkr@gmail.com>
2024-05-15 22:54:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tomers - Rust Cross-Compilation Flake
+
+Tomers is a Nix flake, that, when imported, sets up the right cross-compilation toolchains, dev shells, and builds, based on a user-defined list of targeted platforms.
+
+## Examples
+See https://github.com/thinnerthinker/sursface/blob/main/flake.nix.
---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+  description = "Tomers, a cross-platform Rust flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, crane, fenix, flake-utils, ... }:
+    {
+      libFor = system: targetPlatforms:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          mkPlatform = { system, arch, depsBuild ? [ ], env ? { }, postInstall ? _: "", isDefault ? false }: {
+            name = arch;
+            value = let pi = postInstall; in
+              rec {
+                inherit system;
+                inherit arch;
+                inherit depsBuild;
+                inherit env;
+                inherit isDefault;
+                postInstall = crateName: if isDefault then "" else pi crateName;
+              };
+          };
+
+          mkCraneLib = targetPlatform:
+            let
+              toolchain = with fenix.packages.${system};
+                combine [
+                  latest.rustc
+                  latest.cargo
+                  targets.${targetPlatform.system}.latest.rust-std
+                ];
+            in
+            (crane.mkLib pkgs).overrideToolchain toolchain;
+
+          crateFor = srcLocation: targetPlatform:
+            let
+              craneLib = mkCraneLib targetPlatform;
+            in
+            craneLib.buildPackage
+              ({
+                src = craneLib.cleanCargoSource (craneLib.path srcLocation);
+                cargoVendorDir = craneLib.vendorCargoDeps { cargoLock = ./Cargo.lock; };
+
+                strictDeps = true;
+                doCheck = false;
+
+                CARGO_BUILD_TARGET = targetPlatform.system;
+                depsBuildBuild = targetPlatform.depsBuild;
+
+                postInstall = targetPlatform.postInstall (craneLib.crateNameFromCargoToml { cargoToml = "${srcLocation}/Cargo.toml"; }).pname;
+              } // targetPlatform.env);
+
+          shellFor = srcLocation: targetPlatform:
+            let
+              craneLib = mkCraneLib targetPlatform;
+            in
+            craneLib.devShell ({
+              CARGO_BUILD_TARGET = targetPlatform.system;
+              depsBuildBuild = targetPlatform.depsBuild;
+            } // targetPlatform.env);
+
+          eachPlatform = targetPlatforms: mkFor: pkgs.lib.attrsets.mapAttrs (name: platform: mkFor platform) targetPlatforms // {
+            default = mkFor ((mkPlatform (targetPlatforms.${system} // { isDefault = true; })).value);
+          };
+        in
+        rec {
+          packagesForEachPlatform = srcLocation: eachPlatform targetPlatforms (crateFor srcLocation);
+          devShellsForEachPlatform = srcLocation: eachPlatform targetPlatforms (shellFor srcLocation);
+        };
+    };
+} 
+ 
