646d6050679018f2533c707e05c33b7658cd6cc5
boralg <thinnrthinkr@gmail.com>
2025-04-18 01:15:48 +0300

    feat!: support nightly properly by throwing away the default (stable) toolchain on override

--- flake.nix
+++ flake.nix
@@ -16,89 +16,121 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, crane, fenix, ... }: {
-    libFor = system: targetPlatforms:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib;
+  outputs =
+    {
+      nixpkgs,
+      crane,
+      fenix,
+      ...
+    }:
+    {
+      libFor =
+        system: targetPlatforms:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs) lib;
 
-        mkPlatform =
-          { system
-          , arch
-          , depsBuild ? [ ]
-          , env ? { }
-          , postInstall ? _crateName: ""
-          , buildFilePatterns ? [ ]
-          , isDefault ? false
-          , toolchainPackages ? _fenixPkgs: _crossFenixPkgs: [ ]
-          }: {
-            name = arch;
-            value = let pi = postInstall; in
-              rec {
-                inherit system;
-                inherit arch;
-                inherit depsBuild;
-                inherit env;
-                postInstall = crateName: if isDefault then "" else pi crateName;
-                inherit buildFilePatterns;
-                inherit isDefault;
-                inherit toolchainPackages;
-              };
-          };
-
-        mkCraneLib = targetPlatform:
-          let
-            toolchain = with fenix.packages.${system};
-              combine ([
+          mkPlatform =
+            let
+              defaultToolchain = with fenix.packages.${system}; [
                 stable.rustc
                 stable.cargo
                 targets.${targetPlatform.system}.stable.rust-std
-              ] ++ targetPlatform.toolchainPackages fenix.packages.${system} fenix.packages.${system}.targets.${targetPlatform.system});
-          in
-          (crane.mkLib pkgs).overrideToolchain toolchain;
-
-        crateFor = srcLocation: targetPlatform:
-          let
-            craneLib = mkCraneLib targetPlatform;
-
-            src = lib.cleanSourceWith {
-              src = craneLib.path srcLocation;
-              filter = path: type:
-                (lib.foldl' (acc: p: acc || builtins.match p path != null) false targetPlatform.buildFilePatterns)
-                || (craneLib.filterCargoSources path type);
+              ];
+            in
+            {
+              system,
+              arch,
+              depsBuild ? [ ],
+              env ? { },
+              postInstall ? _crateName: "",
+              buildFilePatterns ? [ ],
+              isDefault ? false,
+              toolchainPackages ? _fenixPkgs: _crossFenixPkgs: defaultToolchain,
+            }:
+            {
+              name = arch;
+              value =
+                let
+                  pi = postInstall;
+                in
+                rec {
+                  inherit system;
+                  inherit arch;
+                  inherit depsBuild;
+                  inherit env;
+                  postInstall = crateName: if isDefault then "" else pi crateName;
+                  inherit buildFilePatterns;
+                  inherit isDefault;
+                  inherit toolchainPackages;
+                };
             };
-          in
-          craneLib.buildPackage ({
-            inherit src;
 
-            strictDeps = true;
-            doCheck = false;
+          mkCraneLib =
+            targetPlatform:
+            let
+              toolchain = fenix.packages.${system}.combine (
+                targetPlatform.toolchainPackages fenix.packages.${system}
+                  fenix.packages.${system}.targets.${targetPlatform.system}
+              );
+            in
+            (crane.mkLib pkgs).overrideToolchain toolchain;
 
-            CARGO_BUILD_TARGET = targetPlatform.system;
-            depsBuildBuild = targetPlatform.depsBuild;
+          crateFor =
+            srcLocation: targetPlatform:
+            let
+              craneLib = mkCraneLib targetPlatform;
 
-            postInstall = targetPlatform.postInstall (craneLib.crateNameFromCargoToml { cargoToml = "${src}/Cargo.toml"; }).pname;
-          } // targetPlatform.env);
+              src = lib.cleanSourceWith {
+                src = craneLib.path srcLocation;
+                filter =
+                  path: type:
+                  (lib.foldl' (acc: p: acc || builtins.match p path != null) false targetPlatform.buildFilePatterns)
+                  || (craneLib.filterCargoSources path type);
+              };
+            in
+            craneLib.buildPackage (
+              {
+                inherit src;
 
+                strictDeps = true;
+                doCheck = false;
 
-        shellFor = srcLocation: targetPlatform:
-          let
-            craneLib = mkCraneLib targetPlatform;
-          in
-          craneLib.devShell ({
-            CARGO_BUILD_TARGET = targetPlatform.system;
-            depsBuildBuild = targetPlatform.depsBuild;
-          } // targetPlatform.env);
+                CARGO_BUILD_TARGET = targetPlatform.system;
+                depsBuildBuild = targetPlatform.depsBuild;
 
-        eachPlatform = targetPlatforms: mkFor: pkgs.lib.attrsets.mapAttrs (name: platform: mkFor platform) targetPlatforms // {
-          default = mkFor ((mkPlatform (targetPlatforms.${system} // { isDefault = true; })).value);
+                postInstall =
+                  targetPlatform.postInstall
+                    (craneLib.crateNameFromCargoToml { cargoToml = "${src}/Cargo.toml"; }).pname;
+              }
+              // targetPlatform.env
+            );
+
+          shellFor =
+            srcLocation: targetPlatform:
+            let
+              craneLib = mkCraneLib targetPlatform;
+            in
+            craneLib.devShell (
+              {
+                CARGO_BUILD_TARGET = targetPlatform.system;
+                depsBuildBuild = targetPlatform.depsBuild;
+              }
+              // targetPlatform.env
+            );
+
+          eachPlatform =
+            targetPlatforms: mkFor:
+            pkgs.lib.attrsets.mapAttrs (name: platform: mkFor platform) targetPlatforms
+            // {
+              default = mkFor ((mkPlatform (targetPlatforms.${system} // { isDefault = true; })).value);
+            };
+
+          platforms = builtins.listToAttrs (map mkPlatform targetPlatforms);
+        in
+        {
+          packagesForEachPlatform = srcLocation: eachPlatform platforms (crateFor srcLocation);
+          devShellsForEachPlatform = srcLocation: eachPlatform platforms (shellFor srcLocation);
         };
-
-        platforms = builtins.listToAttrs (map mkPlatform targetPlatforms);
-      in
-      {
-        packagesForEachPlatform = srcLocation: eachPlatform platforms (crateFor srcLocation);
-        devShellsForEachPlatform = srcLocation: eachPlatform platforms (shellFor srcLocation);
-      };
-  };
+    };
 }
